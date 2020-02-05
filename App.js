c1535e89f8421e846fc6d9fc276c530c48fe08cb
Gavin Quirk <gavinquirk@gmail.com>
2020-02-04 17:12:49 -0800

    header component, listItem component

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item} />}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
